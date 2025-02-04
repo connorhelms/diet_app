5c90451f5a5ed68e912c58b16f886213f4487341
Connor Helms <156145464+connorhelms@users.noreply.github.com>
2025-02-04 08:13:28 -0500

    Delete static directory

--- static/sw.js
+++
@@ -1,8 +0,0 @@
-self.addEventListener('install', function(event) {
-  console.log('Service Worker installing.');
-  // Optionally, cache assets here for offline use.
-});
-
-self.addEventListener('fetch', function(event) {
-  // For now, simply allow normal network requests
-}); 
